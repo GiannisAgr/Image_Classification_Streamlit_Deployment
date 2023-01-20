7c01eeeeb8780bdc121ec3243e2f862fafea40a5
GiannisAgr <101226987+GiannisAgr@users.noreply.github.com>
2023-01-20 03:33:37 +0200

    Add files via upload

---
+++ Vegetable_Classification_Deployment.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import streamlit as st
+import requests
+
+
+def get_prediction_single_image(model, directory):
+    
+    img = tf.keras.preprocessing.image.load_img(directory, target_size=(224, 224, 3))
+    plt.imshow(img)
+    plt.axis("off")
+
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)
+
+    prediction = model.predict(img_array)
+    score = tf.nn.softmax(prediction[0])
+
+    return class_names[np.argmax(score)]
+
+
+class_names = ['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal',
+               'Broccoli', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower',
+               'Cucumber', 'Papaya', 'Potato', 'Pumpkin', 'Radish', 'Tomato']
+
+model = keras.models.load_model("D:\\data_projects\\veggie_classification\\my_model")
+
+
+def get_image(url):
+    img = requests.get(url)
+    file = open('sample_image.jpg', 'wb')
+    file.write(img.content)
+    file.close()
+    img_file_name = 'sample_image.jpg'
+    return img_file_name
+
+st.title('Vegetable Classification')
+st.write('Add image url from the source page')
+
+url = st.text_input("Enter Image Url:")
+if url:
+    image = get_image(url)
+    st.image(image)
+    Classify = st.button("Classify Image")
+    if Classify:
+        st.write("")
+        st.write("Classifying...")
+        label = get_prediction_single_image(model, image)
+        st.write(str(label))
+else:
+    st.write("Paste Image URL")
+
