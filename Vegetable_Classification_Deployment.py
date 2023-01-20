a93d4e2801885dcc2632f0cbe52f6d5b8e88590a
GiannisAgr <101226987+GiannisAgr@users.noreply.github.com>
2023-01-20 15:59:38 +0200

    Update Vegetable_Classification_Deployment.py

--- Vegetable_Classification_Deployment.py
+++ Vegetable_Classification_Deployment.py
@@ -30,10 +30,10 @@ class_names = ['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal',
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    path = os.path.dirname(__file__)
-    my_file = path+'model.pkl'
+#     path = os.path.dirname(__file__)
+#     my_file = path+'model.pkl'
 #     model = tf.keras.models.load_model(my_file, compile=False)
-    model = pickle.load(my_file)
+    model = pickle.load('model.pkl')
     return model
 
 model = load_model()
